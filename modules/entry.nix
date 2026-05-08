33af695f7bc910cf59e72e508daac502fae6441d
Jasper Chan <jasperchan515@gmail.com>
2026-05-08 13:39:13 -0700

    bugfix: inherit inputs into flakeModule

--- modules/entry.nix
+++ modules/entry.nix
@@ -1,9 +1,22 @@
-{ flake-parts-lib, ... }:
+{
+  flake-parts-lib,
+  nixpkgs-lib,
+  uv2nix,
+  pyproject-nix,
+  pyproject-build-systems,
+  ...
+}:
 let
   inherit (flake-parts-lib) importApply;
 in
 {
   flake.flakeModules.default = importApply ./_module.nix {
-
+    inherit
+      flake-parts-lib
+      nixpkgs-lib
+      uv2nix
+      pyproject-nix
+      pyproject-build-systems
+      ;
   };
 }
