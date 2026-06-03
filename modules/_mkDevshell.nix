27355f8624254a46a20d82a9eaa1da5e439fce54
Jasper Chan <jasperchan515@gmail.com>
2026-06-03 02:40:29 -0700

    hack: disable ld_prefix?

--- modules/_mkDevshell.nix
+++ modules/_mkDevshell.nix
@@ -29,7 +29,7 @@
       UV_PYTHON_DOWNLOADS = "never";
     }
     // lib.optionalAttrs pkgs.stdenv.isLinux {
-      LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+      # LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
     };
 
     shellHook =
